6f69346453e174d83c49dcd0851e34eb9b2cdbca
tuanitpro <tuanitpro@gmail.com>
2022-03-17 17:37:23 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,11 +5,6 @@ export default function App({}) {
   return (
     <div>
       Hello
-      <img
-        alt=""
-        src="https://i.postimg.cc/L5HKtS0S/Whats-App-Image-2022-03-14-at-13-50-34.jpg"
-      />
-      <hr />
       <OrderTable />
     </div>
   );
